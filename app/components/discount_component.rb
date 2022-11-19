eb6d39a3fc49a53301bfd20454f6b099d7b4fd04
Zulkuf Demirhan <z.demirhan95@gmail.com>
2022-11-19 23:03:53 +0300

    before_render metod added in course helper for star.png

--- app/components/discount_component.rb
+++ app/components/discount_component.rb
@@ -3,7 +3,12 @@ class DiscountComponent < ViewComponent::Base
   def initialize(item:)
     @item = item
   end
+  
   def render?
     @item.price >= 100
-  end  
+  end
+
+  def before_render
+    @star_icon = helpers.star_icon
+  end
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -3,4 +3,8 @@ module CoursesHelper
     count = (course.price / 4).round()
     tag.p "#{count} enrollees so far"
   end
+
+  def star_icon
+    image_tag("/assets/star.png", width: "1%")
+  end
 end
